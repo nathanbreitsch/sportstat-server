72aa342cb6585cfaf43b4f8f7d4c3755d4204dd6
Brian Breitsch <brianbreitsch@gmail.com>
2015-07-12 16:58:40 -0600

    changed app.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,23 @@
-# init for app (Flask server application)
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+from sportstat.database import init_db, session
+from sportstat.models import Team, Athlete, Game, Play, Action, Observation
+# call to sync models to database
+init_db()
+
+# it is important that all route modules are imported from this `__init__` 
+# file, so they are registered with flask.
+import app.rest
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    '''
+    Flask should automatically call this function when the application shuts
+    down in order to remove all database sessions.
+    '''
+    session.remove()
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='127.0.0.1', port=3000)
---
+++ app/rest.py
@@ -0,0 +1,45 @@
+from app import app
+from sportstat.database import session
+from sportstat.models import Team, Athlete, Game, Play, Action, Observation
+
+
+@app.route('/sportstat/api/v0.1/athletes', methods=['GET'])
+def get_athletes():
+    '''
+    Return all athlete data in json format.
+    '''
+    athletes = [athlete.serialize for athlete in session.query(Athlete)]
+    return jsonify(athletes)
+
+@app.route('/sportstat/api/v0.1/athletes/<last>', methods=['GET'])
+@app.route('/sportstat/api/v0.1/athletes/<first>-<last>', methods=['GET'])
+def get_athlete_by_name(last, first=None):
+    '''
+    Return data in json format for athlete with corresponding name.
+    '''
+    # TODO how to make queries
+    athletes = [athlete.serialize for athlete in session.query(Athlete).filter(first=first, last=last)]
+    return jsonify(athletes)
+
+@app.route('/sportstat/api/v0.1/athletes/<team>-<int:number>', methods=['GET'])
+@app.route('/sportstat/api/v0.1/athletes/<team>-<int:number>-<date>', methods=['GET'])
+def get_athlete_by_team_number_date(team, number, date=None):
+    '''
+    Return data in json format for athlete with corresponding team and number.
+    Optional date parameter can help resolve ambiguity.
+    '''
+    # TODO date and team name validation, handling errors
+    athletes = [athlete.serialize for athlete in session.query(Athlete).filter(team=team, number=number)]
+    return jsonify(athletes)
+
+
+
+@app.route('/sportstat/api/v0.1/teams', methods=['GET'])
+def get_teams():
+    '''
+    Return all team data in json format.
+    '''
+    teams = [team.serialize for team in session.query(Team)]
+    return jsonify(teams)
+
+
